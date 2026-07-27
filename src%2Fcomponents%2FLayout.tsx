9bc9cd71557c46bcaa8db1fd6bca77a32703d0f3
qiyu666 <3270581950@qq.com>
2026-07-27 23:11:55 +0800

    feat: initial commit - full blog project - src/components/Layout.tsx

---
+++ src%2Fcomponents%2FLayout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import MusicPlayer from './MusicPlayer';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+      <MusicPlayer />
+    </>
+  );
+}
